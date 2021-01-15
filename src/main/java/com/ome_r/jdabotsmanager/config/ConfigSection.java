50ccae1f5846f40713c76206bd4b23f2889efa1b
OmerBenGera <omerthepro126@gmail.com>
2021-01-15 22:39:22 +0200

    Added more methods to the config parser

--- src/main/java/com/ome_r/jdabotsmanager/config/ConfigSection.java
+++ src/main/java/com/ome_r/jdabotsmanager/config/ConfigSection.java
@@ -13,29 +13,81 @@ public final class ConfigSection {
         this.data = data;
     }
 
-    public long getLong(String path){
-        return get(path, Long.class);
+    public boolean contains(String path) {
+        return get(path) != null;
     }
 
-    public String getString(String path){
-        return get(path, String.class);
+    public String getString(String path) {
+        return getString(path, null);
+    }
+
+    public String getString(String path, String def) {
+        Object value = get(path);
+        return value == null ? def : value.toString();
+    }
+
+    public int getInt(String path) {
+        return getInt(path, 0);
+    }
+
+    public int getInt(String path, int def) {
+        Object value = get(path);
+        return value instanceof Number ? ((Number) value).intValue() : def;
+    }
+
+    public boolean getBoolean(String path){
+        return getBoolean(path, false);
+    }
+
+    public boolean getBoolean(String path, boolean def){
+        Object value = get(path);
+        return value instanceof Boolean ? (Boolean) value : def;
+    }
+
+    public double getDouble(String path) {
+        return getDouble(path, 0D);
+    }
+
+    public double getDouble(String path, double def) {
+        Object value = get(path);
+        return value instanceof Number ? ((Number) value).doubleValue() : def;
+    }
+
+    public float getFloat(String path) {
+        return getFloat(path, 0f);
+    }
+
+    public float getFloat(String path, float def) {
+        Object value = get(path);
+        return value instanceof Number ? ((Number) value).floatValue() : def;
+    }
+
+    public long getLong(String path) {
+        return getLong(path, 0L);
+    }
+
+    public long getLong(String path, long def) {
+        Object value = get(path);
+        return value instanceof Number ? ((Number) value).longValue() : def;
+    }
+
+    public List<String> getStringList(String path){
+        Object value = get(path);
+        //noinspection unchecked
+        return value instanceof List ? (List<String>) value : new ArrayList<>();
     }
 
     public ConfigSection getSection(String path){
-        //noinspection all
-        return new ConfigSection(get(path, Map.class));
-    }
-
-    @SuppressWarnings("unchecked")
-    public List<String> getStringList(String path){
-        return get(path, ArrayList.class);
+        Object value = get(path);
+        //noinspection unchecked
+        return value instanceof Map ? new ConfigSection((Map<String, Object>) value) : null;
     }
 
     public Set<String> getKeys(){
         return data.keySet();
     }
 
-    private <T> T get(String path, Class<T> type) {
+    private Object get(String path) {
         if (path.contains(".")) {
             String[] split = path.split("\\.");
             String otherPath = path.replace(split[0] + ".", "");
@@ -43,11 +95,10 @@ public final class ConfigSection {
             Object object = data.get(path);
             if(!otherPath.isEmpty() && object instanceof Map)
                 //noinspection unchecked
-                return new ConfigSection((Map<String, Object>) object).get(otherPath, type);
+                return new ConfigSection((Map<String, Object>) object).get(otherPath);
         }
 
-        Object value = data.get(path);
-        return value == null ? null : type.cast(value);
+        return data.get(path);
     }
 
 }
